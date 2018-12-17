7a3e60eb153e4d87eb4ff3fc26fb44aeee5ef923
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-12-17 13:02:22 +0530

    Create 2.py

---
+++ Programs/2.py
@@ -0,0 +1,10 @@
+# Regression
+
+def regression-fitting(a, z):
+  b = a;
+  # Process the data a
+  # Code for regression
+  
+# Sample data for processing
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+z = [9, 8, 7, 6, 5, 4, 3, 2, 1]
